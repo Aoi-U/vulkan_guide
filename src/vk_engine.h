4dfa65c05f7a9383607d1cc460d6a4bed676d452
Aoi Ueki <129098557+Aoi-U@users.noreply.github.com>
2026-09-02 15:44:45 -0600

    Add project files.

---
+++ src/vk_engine.h
@@ -0,0 +1,47 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+class VulkanEngine {
+public:
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+	bool stop_rendering{ false };
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	// vulkan instance handles
+	VkInstance _instance; // vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // gpu chosen as the default device
+	VkDevice _device; // vulkan logical device for comands
+	VkSurfaceKHR _surface; // vulkan window surface
+
+	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+
+private:
+
+	// vulkan initialization functions
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+};
